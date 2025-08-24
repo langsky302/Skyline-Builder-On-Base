6f881b87d26caacea2cb916c189f9b7d0723b29d
langsky302 <langsky302@gmail.com>
2025-08-24 20:49:57 +0700

    Update with Unity Project
    Update with Unity Project

---
+++ Assets/BlockchainScripts/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿public static class GameConfig
+{
+    // Địa chỉ contract ERC721
+    public static readonly string NFTTokenGateContractAddress = "0x2CeE5b9f9385deff373939C064EDb542F6Eec27C";
+
+    // Địa chỉ contract ERC721 khác (nếu khác với TokenGate)
+    public static readonly string NFTVIPContractAddress = "0xCF22fb29C8f830A396597C399F5D3E3B45c3f783";
+
+    // Địa chỉ contract ERC20 - GEM Token
+    public static readonly string TokenGemContractAddress = "0x3cC8B9619c650D55827C925129387ee1D8b7c90b";
+
+}
